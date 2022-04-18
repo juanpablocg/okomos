f77af9c5369e8170a8576fb5f981bce13cc043ac
Juan Pablo <jpablinca@gmail.com>
2022-04-18 18:04:51 -0500

    Creacion del sidebar con AdminLTE

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Okomos')
+
+@section('content_header')
+    <h1>Okomos</h1>
+@stop
+
+@section('content')
+    <p>Sistema Okomos</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
